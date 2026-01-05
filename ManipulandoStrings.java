c327889bbd2c94066710cbc7f6ba54e0ddc174f9
EoDann <danielalmeidasilva337@gmail.com>
2026-01-05 16:31:52 -0300

    Adicionado exercicio de manipulação de Strings

---
+++ ManipulandoStrings.java
@@ -0,0 +1,11 @@
+public class ManipulandoStrings {
+    public static void main(String[] args) {
+        String NumeroString = "5";
+
+        int numero = Integer.parseInt(NumeroString);
+        int resultado = numero + 10;
+
+        System.out.println(resultado);
+    }
+
+}
